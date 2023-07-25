99675e32445d67425ba65f7924ca9b4581758530
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-07-25 20:40:10 -0300

    update!

---
+++ src/application/Program7.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program7 {
+    
+     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos os hospedes vão se hospedar");
+        int h = sc.nextInt();
+        sc.nextLine();
+
+        String[] name = new String[h];
+        String[] email = new String[h];
+        int[] room = new int[h];
+
+        for (int i = 0; i < h; i++) {
+            
+            System.out.println("Rent #" + i);
+            System.out.println("Name: ");
+            name[i] = sc.nextLine();
+            System.out.println("email: ");
+            email[i] = sc.nextLine();
+            System.out.println("Room: ");
+            room[i] = sc.nextInt();
+            sc.nextLine();
+
+
+
+        }
+
+        for (int i = 0; i < h; i++) {
+            System.out.println(room[i] + ": " + name[i] + ", " + email[i]);
+        }
+
+        sc.close();
+     }
+}
--- src/application/program6.java
+++ src/application/program6.java
@@ -36,7 +36,7 @@ public class program6 {
 
         int cont = 0;
         for (int i = 0; i < n; i++) {
-            if(idades[i] < 16){
+            if (idades[i] < 16) {
 
                 cont = cont + 1;
             }
@@ -44,6 +44,11 @@ public class program6 {
         double percent = cont * 100.0 / n;
 
         System.out.printf("Pessoas com menos de 16 anos: %.1f%%%n ", percent);
+
+        for (int i = 0; i < n; i++) {
+            if (idades[i] < 16)
+                System.out.println(nomes[i]);
+        }
 
         sc.close();
 
