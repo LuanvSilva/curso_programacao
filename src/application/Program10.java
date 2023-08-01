5a5b0e2557738a7814c46b009fe4dec15a2e5123
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-08-01 18:25:34 -0300

    Update

--- src/application/Program10.java
+++ src/application/Program10.java
@@ -16,16 +16,16 @@ public class Program10 {
         Instant d03 = Instant.now();
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
-        LocalDate d04 = LocalDate.parse("26/07/1996T01:30:26", fmt);
-        LocalDate d05 = LocalDate.parse("26/07/1996T01:30:26");
+        LocalDate d04 = LocalDate.parse("26/07/1996", fmt);
         
+        //////
 
         System.out.println("d01 = " + d01);
         System.out.println("d02 = " + d02);
         System.out.println("d03 = " + d03);
         System.out.println("d04 = " + d04);
         System.out.println("fmt = " + d04.format(fmt));
-        System.out.println("fmt = " + fmt2.format(d05));
+        
 
     }
 }
