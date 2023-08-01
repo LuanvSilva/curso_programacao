9e92fa430ea56308e90ce0936cb29591a268858d
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-08-01 18:47:08 -0300

    !

--- src/application/Program10.java
+++ src/application/Program10.java
@@ -18,15 +18,13 @@ public class Program10 {
         DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
         LocalDate d04 = LocalDate.parse("26/07/1996", fmt);
 
-        
-        ///////////
-
+ 
         System.out.println("d01 = " + d01);
         System.out.println("d02 = " + d02);
         System.out.println("d03 = " + d03);
         System.out.println("d04 = " + d04);
         System.out.println("fmt = " + d04.format(fmt));
         
-
+///
     }
 }
