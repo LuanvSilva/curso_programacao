8bf18a1ca51852f4d01209ba3a6e623b7becc2f8
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-08-03 19:09:05 -0300

    update

--- src/application/Program11.java
+++ src/application/Program11.java
@@ -9,6 +9,10 @@ public class Program11 {
 
     public static void main(String[] args){
 
-        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT) 
+
+        Order order = new Order(1080, (java.sql.Date) new Date(), OrderStatus.PENDING_PAYMENT);
+
+        System.out.println(order);
+    
     }
 }
